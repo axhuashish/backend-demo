ef65652823dd47286e80e91a164cbfbdce8b1d5a
axhuashish <93907760+axhuashish@users.noreply.github.com>
2026-04-27 21:51:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const SECRET = process.env.JWT_SECRET || "defaultsecret";
 
-// Home route
 app.get("/", (req, res) => {
   res.send("Backend is running with env variables");
 });
 
-// Secret route
 app.get("/secret", (req, res) => {
   res.send("Secret is: " + SECRET);
 });
 
-// Optional test route
 app.get("/test", (req, res) => {
-  res.send("CI/CD is working 🚀");
+  res.send("CI/CD is working");
 });
 
-// Server start
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
