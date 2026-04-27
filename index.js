77bc2f8aca59f591aa6d9e279632db34e07b2c87
axhuashish <93907760+axhuashish@users.noreply.github.com>
2026-04-27 18:37:55 +0530

    final env fix

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const app = express();
 
+// PORT (Render automatically set karega)
 const PORT = process.env.PORT || 3000;
 
+// ENV variable (IMPORTANT)
 const SECRET = process.env.JWT_SECRET || "defaultsecret";
 
+// Home route
 app.get("/", (req, res) => {
   res.send("Hello, Backend is Working!");
 });
 
+// Secret route
 app.get("/secret", (req, res) => {
   res.send("Secret is: " + SECRET);
 });
 
+// Server start
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
